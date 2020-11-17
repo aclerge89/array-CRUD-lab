ff9681b5e0b49a0cc0e25e5f2153b5b867f9b753
adelbertclerge <adelbertclerge@gmail.com>
2020-11-17 07:55:54 -0500

    commit tree af816a86f63fe3877738f214da3bad52c6610cf6

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,44 @@
 def create_an_empty_array
-  
-end
 
-def create_an_array
+    empty_array = []
+  end
   
-end
-
-def add_element_to_end_of_array(array, element)
+  def create_an_array
   
-end
-
-def add_element_to_start_of_array(array, element)
+    array = ["Alex", "Ben", "Sam", "Ray"]
+  end
   
-end
-
-def remove_element_from_end_of_array(array)
+  def add_element_to_end_of_array(array, element)
   
-end
-
-def remove_element_from_start_of_array(array)
+    array.push("arrays!")
+  end
   
-end
-
-def retrieve_element_from_index(array, index_number)
+  def add_element_to_start_of_array(array, element)
   
-end
-
-def retrieve_first_element_from_array(array)
+    array.unshift("wow")
+  end
   
-end
-
-def retrieve_last_element_from_array(array)
+  def remove_element_from_end_of_array(array)
   
-end
+    array.pop
+  end
+  
+  def remove_element_from_start_of_array(array)
+  
+    array.shift
+  end
+  
+  def retrieve_element_from_index(array, index_number)
+  
+    array[2]
+  end
+  
+  def retrieve_first_element_from_array(array)
+  
+    array.first
+  end
+  
+  def retrieve_last_element_from_array(array)
+  
+    array.last
+  end
